89412adb47011485dd1208a6112c4a0a41397df1
Aitor PC <aitorpalomares@hotmail.com>
2019-03-17 20:58:24 +0000

    storybook configurated

---
+++ src/components/ui/Avatar/Avatar.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Avatar from '.';
+
+storiesOf('Avatar', module)
+  .add('Basic', () => <Avatar />);
---
+++ src/components/ui/Divider/Divider.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Divider from './index';
+
+storiesOf('Divider', module)
+  .add('Basic', () => <Divider />);
